37fc2f4bd9aa5d33ea552062abfb7080ae1f7705
(none) <(none)>
2025-06-17 12:57:07 +0000

    fixed two numbers

--- calc.py
+++ calc.py
@@ -3,4 +3,13 @@
 def add(numbers):
     if numbers == "": return 0
     if numbers == "1,2": return 3
-    return int(numbers)
+    numbers += '='
+    nums = []
+    current_num = ''
+    for i in range(len(numbers)):
+        if numbers[i].isdigit():
+            current_num += numbers[i]
+        elif current_num != '':
+            nums.append(int(current_num))
+            current_num = ''
+    return sum(nums)
